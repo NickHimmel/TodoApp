117d4cae33fcd4dd95360d6bdf8e322f78efdbfb
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-11-22 14:48:44 -0500

    Add TodoSearch component

---
+++ app/components/TodoSearch.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react;'
+
+class TodoSearch extends Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>Search Todos </div>
+      )
+    }
+}
+
+export default TodoSearch
